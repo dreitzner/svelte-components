ec4d861118a0075321d1a84502c54ba2bcea8ff4
§Domenik Reitzner <domenik@reitzner.at>
2020-01-07 14:57:10 +0100

    add validation summary

---
+++ ValidationSummary.svelte
@@ -0,0 +1,35 @@
+<script>
+import { afterUpdate } from 'svelte';
+export let validationSummaryData;
+let summaryEl;
+
+afterUpdate(() => {
+    if (!summaryEl) return;
+    summaryEl.querySelector('li').focus();
+});
+</script>
+
+{#if validationSummaryData.length}
+<ul bind:this={summaryEl}>
+    <div class="validationSummaryHeadline">The following errors occured:</div>
+    {#each validationSummaryData as data}
+    <li>
+        <a href="#{data.id}">{data.errorMessage}</a>
+    </li>
+    {/each}
+</ul>
+{/if}
+
+<style type="text/scss">
+ul {
+    border: 1px solid red;
+    color: red;
+    background: #fae3e5;
+}
+ul > * {
+    padding: 5px 10px;
+}
+a {
+    color: red;
+}
+</style>
